d3b7edabd0aa398909d97fc27459431d76f03bfd
Daniel Koefoed <dc.koefoed@gmail.com>
2021-09-29 10:34:20 +0200

    inital db setup and testing

--- back/app.js
+++ back/app.js
@@ -7,6 +7,20 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect(process.env.DB_HOST);
+}
+
+const Test = require('./models/Test');
+
+const shit = new Test({ name: 'Shit' });
+console.log(shit.name);
 
 app.use(logger('dev'));
 app.use(express.json());
