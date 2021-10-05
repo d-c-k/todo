dbaddf7a4ebcdda52b292ec6f064cb054f22bdc7
d-c-k <dc.koefoed@gmail.com>
2021-10-05 10:35:53 +0200

    install and test cors npm-package

--- back/app.js
+++ back/app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
 require('dotenv').config()
 
 const mongoose = require('mongoose');
@@ -24,6 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
