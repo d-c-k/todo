4b387fcf38cb6ec008b421cf118ab092d0edfea3
d-c-k <dc.koefoed@gmail.com>
2021-10-05 14:32:34 +0200

    fetch data to homepage

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
 export default function HomePage() {
+  const [todoItems, setTodoItems] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:1337/api/items', {
+      headers: {
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => setTodoItems(item => [...data]))
+  }, []);
+
+  console.log(todoItems);
   return (
     <>
       <p>Home</p>
