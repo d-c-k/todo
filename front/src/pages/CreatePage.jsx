6af2386ef63b8ad9eb5ac3755963f4f327d559ef
d-c-k <dc.koefoed@gmail.com>
2021-10-06 18:09:04 +0200

    POST from react to DB works

--- front/src/pages/CreatePage.jsx
+++ front/src/pages/CreatePage.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom'; 
 
 export default function CreatePage() {
-//  function handleOnSubmit(e){
-//    e.preventDefault()
-//    const url = 'http://localhost:1337/api/items';
-//    fetch(url, {
-//      method: 'POST',
-//      body: JSON.strigify(formData),
-//      headers: {
-//        "Content-Type":"application/json"
-//      }
-//    })
-//    .then(res => res.json())
-//    .then(data => history.push('/home'))
-//  }
+  const [formInput, setFormInput] = useState({});
+  const history = useHistory();
+
+  function handleChange(e){
+    setFormInput({...formInput, [e.target.name]: e.target.value});
+  };
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const url = 'http://localhost:1337/api/items';
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(formInput),
+      headers: {
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => history.push('/home'))
+  };
 
   return (
     <>
       <p>Create new item</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" onChange={handleChange} />
+      <textarea name="body" value={formInput.body} onChange={handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   ) 
 };
