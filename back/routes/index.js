c7e7840b43246a451120564273deae67e31b5a69
d-c-k <dc.koefoed@gmail.com>
2021-10-07 10:52:58 +0200

    start rework of routes to thenables

--- back/routes/index.js
+++ back/routes/index.js
@@ -10,27 +10,38 @@ router.get('/api/items', async(req, res, next) => {
     res.statusMessage = 'Not found';
     res.end('404 - Not found');
   }else{
+    res.statusCode = 200;
     res.json(items);
   }
 })
 
-router.get('/api/items/:id', async(req, res, next) => {
+router.get('/api/items/:id', (req, res, next) => {
   const id = req.params.id;
-  if(!id){
-    res.statusCode = 500;
-    res.statusMessage = 'Invalid ID';
-    res.end('500 - Invalid ID')
-  }else{
-    const item = await Item.findOne({_id:id});
-    if(!item){
-      res.statusCode = 404;
-      res.statusMessage = 'Not found';
-      res.end('404 - Not found');
-    }else{
-      res.json(item);
-    }
-  }
+  Item.findOne({_id:id})
+    .then((data) => {
+      res.json(data);
+      res.sendStatus(200);
+    })
+    .catch(() => res.sendStatus(400));
 })
+//router.get('/api/items/:id', async(req, res, next) => {
+//  const id = req.params.id;
+//  if(!id){
+//    res.statusCode = 500;
+//    res.statusMessage = 'Invalid ID';
+//    res.end('500 - Invalid ID')
+//  }else{
+//    const item = await Item.findOne({_id:id}, function(err, result) {});
+//    if(!item){
+//      res.statusCode = 404;
+//      res.statusMessage = 'Not found';
+//      res.end('404 - Not found');
+//    }else{
+//      res.statusCode = 200;
+//      res.json(item);
+//    }
+//  }
+//})
 
 router.post('/api/items', async(req, res, next) => {
   const { title, body } = req.body;
